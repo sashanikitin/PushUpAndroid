78c1c5ca827a34da45205794c1d5c1dab5cff8a2
Oleksandr Nikitin <supersashanikitin@gmail.com>
2025-09-02 07:14:34 +0200

    Play with compose

--- app/src/main/java/sport/pushup/MainActivity.kt
+++ app/src/main/java/sport/pushup/MainActivity.kt
@@ -3,11 +3,8 @@ package sport.pushup
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -26,14 +23,42 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = Color.Green
+                    color = Color.LightGray
                 ) {
-                    Greeting("Android")
+                    IntrinsicTest()
+                    //Greeting("Android")
                 }
             }
         }
     }
 }
+
+@Composable
+fun IntrinsicTest() {
+    Row (Modifier.height(56.dp)){
+        Button(
+            onClick = {}) {
+            Text("button\n1")
+
+        }
+
+        Button(
+            onClick={}, ) {
+          //  modifier = Modifier.fillMaxHeight()) {
+            Text("button2")
+        }
+    }
+    Spacer(modifier = Modifier.width(50.dp))
+    Row (Modifier.height(800.dp)) {
+        Button(
+            onClick = {}) {
+            Text("Big button\n1")
+
+        }
+    }
+}
+
+
 
 @Composable
 fun Greeting(name: String) {
